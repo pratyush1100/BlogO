ee1a40e0d2dc2e2553b8030995d954979d4ee6b1
ayush kumar <srivastava16ayush@gmail.com>
2024-10-02 05:59:00 +0530

    cors error solved

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://blogo-frontend.onrender.com',
     credentials: true
 }));
 app.use(express.json());
